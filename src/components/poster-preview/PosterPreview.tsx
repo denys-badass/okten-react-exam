646271729cae5fbaa550fbbed1a1120c478b1a69
Denys Podobrii <denys.podobrii@gmail.com>
2025-05-03 18:04:02 +0300

    add poster preview

--- src/components/poster-preview/PosterPreview.tsx
+++ src/components/poster-preview/PosterPreview.tsx
@@ -14,8 +14,8 @@ export const PosterPreview: FC<PosterProp> = ({movie, handlerClick}) => {
             className='fixed top-[10vh] left-0 z-10 w-full h-[90dvh] flex justify-center items-center'
             onClick={handlerClick}>
             <img
-                className='z-20'
-                src={import.meta.env.VITE_IMAGE_BASE_URL + poster_path}
+                className='z-20 h-[80dvh]'
+                src={import.meta.env.VITE_IMAGE_BASE_URL + '/w780' + poster_path}
                 alt={title}
             />
         </div>
